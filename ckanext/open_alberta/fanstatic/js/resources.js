89a2eb15c7bea25ffa9521d430f670f7d46f6a5c
Greg Burek <grzegorz.burek@gov.ab.ca>
2017-02-07 11:54:01 -0700

    1. Modified the page and home layouts so the content top has 100% width white background. 2. Included mobile filters panel changes from another commit and adjusted the behaviour/styling.

--- ckanext/open_alberta/fanstatic/js/resources.js
+++ ckanext/open_alberta/fanstatic/js/resources.js
@@ -1,6 +1,6 @@
 function hide_filters_on_body_click() {
     $('body').off('click', hide_filters_on_body_click);
-    $('.wrapper-resources div.secondary').animate({'margin-left': '-350px'}, 300);
+    $('.mobile-filters-panel').animate({'margin-left': '-350px'}, 300);
 }
 
 $(function() {
@@ -14,30 +14,22 @@ $(function() {
 
     // Move the filters panel to the main column and shift it to the left so it's ready to slide in.
     // Mobile view only.
-    if ($('#show-filters').css('display') != 'none') {
-        $('.wrapper-resources div.secondary')
-            .addClass('mobile-filters-panel')
-            .appendTo('.wrapper-resources > .primary')
-            /*.css({'position': 'absolute',
-                  'top': '0px',
-                  'left': '0px',
-                  'padding': '0px',
-                  'margin-left': '-350px'}) */
-            .click(function(e) { 
-                // prevent tap/click on filters panel to get to the body element
-                e.stopPropagation(); 
+    var mfp = $('.catalogue div.secondary').clone();
+    mfp.prependTo('body')
+       .addClass('mobile-filters-panel')
+       .show()
+       .click(function(e) { 
+           // prevent tap/click on filters panel to get to the body element
+           // e.stopPropagation(); 
+       });
+
+    $('#show-filters').click(function() {
+        $('.mobile-filters-panel')
+            .show()
+            .animate({'margin-left': 0}, 300, function() {
+                $('body').on('click', hide_filters_on_body_click);
             });
+    });
 
-        $('#show-filters').click(function() {
-            $('.wrapper-resources div.secondary')
-                .animate({'margin-left': 0}, 300, function() {
-                    $('body').on('click', hide_filters_on_body_click);
-                });
-        });
-
-        //$('h3.filters-header .fa-close').click(function() {
-        //    $('.wrapper-resources div.secondary').animate({'margin-left': '-350px'}, 300);
-        //});
-        $('h3 .fa-close').click(hide_filters_on_body_click);
-    }
+    $('h3 .fa-close').click(hide_filters_on_body_click);
 });
